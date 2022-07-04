523a6e7d31ec1aaab8778346c68ffb7929324845
Fabrizio <fmoatte@gmail.com>
2022-07-04 19:28:59 -0400

    add a dinamic way to set the grid

--- Etch-a-Sketch/main.js
+++ Etch-a-Sketch/main.js
@@ -1,25 +1,60 @@
 const mContainer = document.querySelector(".main-container");
-const reiniciar = document.querySelector(".nav");
+const reiniciar = document.querySelector(".reset");
+const select = document.querySelector(".select");
+let numeroColumns = 4096;
 
-for(let i=0;i<100;i++){
-    let board = document.createElement("div");
-    board.classList.add("cDivs");
-    mContainer.appendChild(board)    
+// funtion that executes the default grid and the primary functions of the etch-a-sketch
+const grilla = () => {
+
+    // setting the large of the div, by default its setted to 4096 (64x64), and in the .main-container css class the grid column template is setted to 64. 
+    for(let i=0;i<numeroColumns;i++){
+        let board = document.createElement("div");
+        board.classList.add("cDivs");
+        mContainer.appendChild(board)    
+    }
+
+    // adding a cDivsH class to the divs that the mouse is over. this is like painting the divs.
+    mContainer.childNodes.forEach( e =>{
+        e.addEventListener("mouseover", k =>{
+            k.target.classList.add("cDivsH")
+        })
+    })
+    
+    // return all divs to blank, removing cDivsH
+    reiniciar.addEventListener("click", e=>{
+        mContainer.childNodes.forEach(k =>{
+            k.classList = "cDivs"
+        })
+            
+    })
 }
 
-mContainer.childNodes.forEach( e =>{
-    e.addEventListener("mouseover", k =>{
-        k.target.classList.add("cDivsH")
-    })
-})
+grilla(); // initialize the grilla function
+
+// function that removes all the children nodes of the parent
+const removeAllChildren = ()=>{
+    while (mContainer.firstChild) {
+        mContainer.removeChild(mContainer.firstChild);
+    }
+}
+
+// function that resets the grid display and large, changing the numeroColumns and the grid template columns in the css class
+const gridColumns = e => {
+    removeAllChildren();
+    numeroColumns = +prompt("Ingrese el largo de la grilla");
+    mContainer.style.gridTemplateColumns = `repeat(${numeroColumns}, 1fr)`;
+    numeroColumns = numeroColumns * numeroColumns;
+    grilla();   
+}
+
+// executes gridColumns funtion when clicking the select button
+select.addEventListener("click", gridColumns);
 
 
-reiniciar.addEventListener("click", e=>{
-    mContainer.childNodes.forEach(k =>{
-        k.classList = "cDivs"
-    })
-        
-})
+
+
+
+
 
 
 
